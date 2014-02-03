c1198f99197f27bc5882a8876894d04cf5abc98a
moritz640 <moritz.wenzel@secure-mail.biz>
2014-02-03 18:16:42 +0100

    Update hanoi7.c

--- hanoi7.c
+++ hanoi7.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[]) //In the next version I will use separate functions to make more manageable the Main function.
 {
   while(1)
   {
